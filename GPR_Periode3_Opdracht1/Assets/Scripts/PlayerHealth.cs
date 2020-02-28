5ce47faf1882b0be0c0a5d3c8cd93114f74b32ea
Wayne Hofstra <wayne.hofstra@hva.nl>
2020-02-28 09:36:51 +0100

    Initialize

---
+++ GPR_Periode3_Opdracht1/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    private int _health;
+
+    [SerializeField]
+    private Text _healthText;
+
+    private void Start()
+    {
+        _healthText.text = "Health: " + _health;
+    }
+
+    private void Update()
+    {
+        if (_health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        _health -= 10;
+        _healthText.text = "Health: " + _health;
+    }
+}
